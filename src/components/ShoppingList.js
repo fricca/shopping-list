4a98c812423b7bf86c47a365b58711d67d6b7049
fricca <fricca@uk2.net>
2020-03-03 15:43:28 +0100

    Categorize item list
    * output list of shopping items grouped by category

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -8,29 +8,76 @@ class ShoppingList extends Component {
         shoppingListItems: PropTypes.arrayOf(shoppingItemPropType),
     };
 
-    render() {
-        const { shoppingListItems } = this.props;
+    categorizeItems = () => {
+        const { shoppingListItems: items } = this.props;
+        const categorized = {};
+        this.props.categories.forEach(category => {
+            if (items.find(item => item.category === category.id)) {
+                categorized[category.id] = items.filter(
+                    item => item.category === category.id
+                );
+            }
+        });
 
-        if (shoppingListItems.length) {
-            return (
-                <ul>
-                    {shoppingListItems.map(item => (
-                        <li key={item.id}>
-                            <ShoppingListItem
+        return categorized;
+    };
+
+    renderItemList = items => {
+        return (
+            <ul className="item-list__list">
+                {items.map(item => (
+                    <li key={item.id} className="item-list__item">
+                        <ShoppingListItem
                             categories={this.props.categories}
-                                shoppingListItem={item}
-                                markAsBought={this.props.markAsBought}
-                                deleteShoppingListItem={
-                                    this.props.deleteShoppingListItem
-                                }
-                            />
-                        </li>
-                    ))}
-                </ul>
-            );
-        }
+                            shoppingListItem={item}
+                            markAsBought={this.props.markAsBought}
+                            deleteShoppingListItem={
+                                this.props.deleteShoppingListItem
+                            }
+                        />
+                    </li>
+                ))}
+            </ul>
+        );
+    };
 
-        return "No Items Yet";
+    render() {
+        const { shoppingListItems: items, categories } = this.props;
+        const categorizedItems = this.categorizeItems();
+
+        const content = items.length ? (
+            <ul className="cat-list__list">
+                {Object.keys(categorizedItems).map(catKey => {
+                    const currentCat = categories.find(
+                        cat => cat.id === catKey
+                    );
+                    if (currentCat) {
+                        return (
+                            <li className="cat-list__item" key={catKey}>
+                                <h2 className="cat-list__title">
+                                    {currentCat.symbol} {currentCat.name}
+                                </h2>
+                                <div className="cat-list__content item-list">
+                                    {this.renderItemList(
+                                        categorizedItems[catKey]
+                                    )}
+                                </div>
+                            </li>
+                        );
+                    }
+
+                    return null;
+                })}
+            </ul>
+        ) : (
+            <p className="cat-list__empty">No items yet</p>
+        );
+
+        return (
+            <div className={"cat-list" + (!items.length ? " is-empty" : "")}>
+                {content}
+            </div>
+        );
     }
 }
 
